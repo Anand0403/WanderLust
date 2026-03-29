b5060e02b51866cf0df7801d7ce37935c3a8b95f
Anand0403 <anandshiremani123@gmail.com>
2026-03-29 15:41:33 +0530

    Initial commit - StayEase project setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+main().then(() => {
+    console.log("Connected to DB");
+}).catch((err) => {
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+}
+
+app.get("/",(req,res) => {
+    res.send("Hi i am root");
+
+});
+
+app.listen(8080, () => {
+    console.log("Server is listening to port 8080");
+});
